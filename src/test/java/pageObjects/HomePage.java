f76e7018035e746bcb4942171dcc1e2d80167b7e
Bhoomika N <gbhoomika18@gmail.com>
2025-09-11 23:27:43 +0530

    Initial commit - folder structure

---
+++ src/test/java/pageObjects/HomePage.java
@@ -0,0 +1,15 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class HomePage extends BasePage{
+	//WebDriver driver;
+
+	public HomePage(WebDriver driver) {
+		super(driver);
+	}
+	
+	
+	
+	
+}
